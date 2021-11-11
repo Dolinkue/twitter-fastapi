d5d9c720821c5794fdf4280f2061d4d59c3e69b3
nico <dolinkue_n@hotmail.com>
2021-11-11 11:39:27 -0300

    modelos_usuarios

--- main.py
+++ main.py
@@ -1,7 +1,55 @@
+# Python
+from uuid import UUID
+from datetime import date
+from datetime import datetime
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# FastAPI
 from fastapi import FastAPI
 
-app =FastAPI()
+app = FastAPI()
+
+# Models
+# UUID es una identificacion que provee python para identificar los usuarios
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ..., 
+        min_length=8
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ..., 
+        min_length=1, 
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 @app.get(path="/")
 def home():
-    return{"twitter API": "Working"}
+    return {"Twitter API": "Working!"}
